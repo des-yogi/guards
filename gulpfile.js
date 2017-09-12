9955f6e813757f4d54757e2c9b9dc96bd1676d57
des-yogi <nuevo-design@mail.ru>
2017-09-12 19:26:47 +0300

    in progress

--- gulpfile.js
+++ gulpfile.js
@@ -104,7 +104,7 @@ gulp.task("symbols", function() {
 
 gulp.task("copy", function() {
   return gulp.src([
-    "fonts/**/*.{woff,woff2}",
+    "fonts/**/*.{woff,woff2,otf,ttf,eot}",
     "img/**",
     "js/**",
     "*.html",
